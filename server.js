e2474e5a7bcdbc26b09482f00f047ccabe990492
pujasodha <34947997+pujasodha@users.noreply.github.com>
2018-04-30 21:22:35 -0700

    html route and server js set up

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+var PORT = process.env.PORT || 8080
+
+var db = require('./models')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+
+require('./routes/api-routes.js')(app)
+require('./routes/html-routes')(app)
+
+db.sequelize.sync({ force: true }).then(function(){
+    app.listen(PORT, function() {
+        console.log("http://localhost:" + PORT)
+    })
+})
