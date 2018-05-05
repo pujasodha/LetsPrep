c7e0b5b3152b25470ece20ea8046db4cc2d5a6f1
chrisyshorshort <chritinasarahwu@gmail.com>
2018-05-05 10:46:52 -0700

    added more info

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,8 @@
     M.AutoInit();
     $('.sidenav').sidenav();
     $('.modal').modal();
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 100,
+    })
+    $('select').material_select();
   });
