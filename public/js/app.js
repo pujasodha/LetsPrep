231fe3464d26da7b1cd605a94ef20df4dcaa584b
pujasodha <pujasodha@gmail.com>
2018-05-07 21:35:34 -0700

    modal working

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
  $(document).ready(function () {
-     //     M.AutoInit();
-     //     $('.sidenav').sidenav();
-     //     $('.modal').modal();
-     //     $('.scrollspy').scrollSpy({
-     //         scrollOffset: 100,
-     //     })
-     //     $('select').material_select();
+         M.AutoInit();
+         $('.sidenav').sidenav();
+         $('.modal').modal();
+         $('.scrollspy').scrollSpy({
+             scrollOffset: 100,
+         })
+         $('select').material_select();
 
 
      //  ======================= API and AJAX Calls =========================== //
